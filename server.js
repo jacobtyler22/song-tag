a5f0f92568c57323298c368862b4a093676d48ef
Jacob Tyler <jacobtyler22@icloud.com>
2015-02-13 15:47:09 -0700

    Update

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
 var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var artistCtrl = require('./lib/controllers/artistCtrl.js');
+var songCtrl = require('./lib/controllers/songCtrl.js');
+var tagCtrl = require('./lib/controllers/tagCtrl.js');
 
 var app = express();
+var mongoUri = "mongodb://localhost:27017/song-tag";
 
-//routes go here
+mongoose.connect(mongoUri);
+mongoose.connection.once('open', function(){
+	console.log('Connected to database at ' + mongoUri);
+});
 
-app.listen(8000);
+app.use(bodyParser.json());
+
+app.post('/artist', artistCtrl.post);
+app.post('/song', songCtrl.post);
+app.post('/tag', tagCtrl.post);
+app.get('/artist', artistCtrl.get);
+app.get('/song', songCtrl.get);
+app.get('/tag', tagCtrl.get);
+
+app.listen(8000, function(){
+	console.log('Listening on port 8000');
+});
